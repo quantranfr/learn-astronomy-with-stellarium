4446e2c5b84783c8f8cc75a1ffd827244136c27f
quantranfr <53756024+quantranfr@users.noreply.github.com>
2024-03-03 08:18:13 +0700

    simplify

--- docs/authentication.js
+++ docs/authentication.js
@@ -5,8 +5,8 @@ function initializeNetlifyIdentity() {
     if (window.netlifyIdentity) {
         window.netlifyIdentity.on("init", user => {
             if (!user) {
-                // If user is not logged in, display the login modal
-                window.netlifyIdentity.open();
+                // If user is not logged in, redirect to login page
+                window.location.href = "/login.html";
             }
         });
         // Trigger Netlify Identity initialization
@@ -16,19 +16,8 @@ function initializeNetlifyIdentity() {
 
 // Function to execute on DOMContentLoaded event
 function onDOMContentLoaded() {
-    // Include Netlify Identity widget script
-    var script = document.createElement("script");
-    script.src = "https://identity.netlify.com/v1/netlify-identity-widget.js";
-    script.onload = function() {
-        // Initialize Netlify Identity after the script has loaded
-        initializeNetlifyIdentity();
-    };
-    document.head.appendChild(script);
-    
-    // Add the Netlify Identity menu div
-    var identityMenuDiv = document.createElement("div");
-    identityMenuDiv.setAttribute("data-netlify-identity-menu", "");
-    document.body.appendChild(identityMenuDiv);
+    // Initialize Netlify Identity
+    initializeNetlifyIdentity();
 }
 
 // Event listener for DOMContentLoaded event
