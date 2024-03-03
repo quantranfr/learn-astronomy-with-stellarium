8446232facadbc6ebbf93aa891fd28f66cef2589
quantranfr <53756024+quantranfr@users.noreply.github.com>
2024-03-03 07:48:54 +0700

    test netlify identity

---
+++ docs/authentication.js
@@ -0,0 +1,33 @@
+// authentication.js
+
+// Function to initialize Netlify Identity
+function initializeNetlifyIdentity() {
+    if (window.netlifyIdentity) {
+        window.netlifyIdentity.on("init", user => {
+            if (!user) {
+                // If user is not logged in, display the login modal
+                window.netlifyIdentity.open();
+            }
+        });
+    }
+}
+
+// Function to execute on DOMContentLoaded event
+function onDOMContentLoaded() {
+    // Include Netlify Identity widget script
+    var script = document.createElement("script");
+    script.src = "https://identity.netlify.com/v1/netlify-identity-widget.js";
+    script.onload = function() {
+        // Initialize Netlify Identity after the script has loaded
+        initializeNetlifyIdentity();
+    };
+    document.head.appendChild(script);
+    
+    // Add the Netlify Identity menu div
+    var identityMenuDiv = document.createElement("div");
+    identityMenuDiv.setAttribute("data-netlify-identity-menu", "");
+    document.body.appendChild(identityMenuDiv);
+}
+
+// Event listener for DOMContentLoaded event
+document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
